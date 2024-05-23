8967bdc1e4cdeabca9e8e97cc36f7d6a70368af2
Stry12 <142692315+Stry12@users.noreply.github.com>
2024-05-23 15:34:51 -0400

    Ejemplo de acceso a datos de fila en eliminar

--- dhermesfront/src/pages/Inventario/Inventario.jsx
+++ dhermesfront/src/pages/Inventario/Inventario.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import Tabla from '../../components/TablaGenerica/Tabla';
 
-const encabezados = ['Nombre', 'Cantidad', 'Tipo', 'Ingreso', 'Vencimiento', 'Estado', 'Acciones'];
+const encabezados = ['ID','Nombre', 'Cantidad', 'Tipo', 'Ingreso', 'Vencimiento', 'Estado', 'Acciones'];
 const datos = [
-  ['Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
-  ['Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Vencido'],
-  ['Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
-  ['Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Proximo'],
-  ['Artismi', 50, 'Jarabe', '20/02/2002', '20/02/2022', 'Valido'],
-  ['Artismi2', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
-  ['Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
-  ['Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
-  ['Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
-  ['Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
-  ['Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido']
+  [1,'Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
+  [2,'Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Vencido'],
+  [3,'Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
+  [4,'Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Proximo'],
+  [5,'Artismi', 50, 'Jarabe', '20/02/2002', '20/02/2022', 'Valido'],
+  [6,'Artismi2', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
+  [7,'Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
+  [8,'Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
+  [9,'Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
+  [10,'Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido'],
+  [11,'Artismi', 50, 'Pastilla', '20/02/2002', '20/02/2022', 'Valido']
 ];
 
 function Inventario() {
 
-  const handleEliminarClick = () => {
-    console.log('Eliminar');
+  const handleEliminarClick = (rowIndex) => {
+    console.log(datos[rowIndex][0]);
   };
   
-  const handleEditarClick = () => {
+  const handleEditarClick = (rowIndex) => {
     console.log('Editar');
   };
 
-  const handleNuevoMedicamentoClick = () => {
+  const handleNuevoMedicamentoClick = (rowIndex) => {
     console.log('Nuevo medicamento');
   }
 
